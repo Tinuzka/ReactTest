1d84c1ba41d84fdb67ac91b6f0ef14c7b7ac85b2
Tiina Mononen <tiina.m.mononen@gmail.com>
2017-08-20 21:37:41 +0300

    Removed unneeded code

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Question from './ModelObjects/Question';
 import Answer from './ModelObjects/Answer';
 import registerServiceWorker from './registerServiceWorker';
 
-var testAnswer = new Answer('test', 10);
 var question = new Question('Mitä teet aamulla', [new Answer('Mietit maailman valloitusta', 1, 's'), new Answer('Nouset ahkerana päivän toimiin', 1, 'h'), new Answer('Valmistaudut rohkeasti päivän haasteisiin', 1, 'g'), new Answer('Mietit yhä sitä ongelmaa, mihin et keksinyt eilen ratkaisua', 1, 'r')]);
 var question2 = new Question('Lempi eläimesti', [new Answer('Käärme', 1, 's'), new Answer('Mäyrä', 1, 'h'), new Answer('Leijona', 1, 'g'), new Answer('Kotka', 1, 'r')]);
 
